2a55ebb349b652ad92ec3d8a368f8ff2cad320dc
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-08-20 15:10:06 -0400

    More verbose installation script output

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+COLOR_NC='\033[0m'
+COLOR_GREEN='\033[0;32m'
+COLOR_YELLOW='\033[0;33m'
+
 EXCLUDE=(install.sh README.markdown)
 
 for filename in $(ls "$(dirname $0)"); do
   [[ ${EXCLUDE[@]} =~ $filename ]] && continue
   original="$(cd "$(dirname $0)" && pwd)/$filename"
   symbolic="$HOME/.$filename"
+
   if [ -e "$symbolic" ]; then
     if [[ $(readlink "$symbolic") != "$original" ]]; then
-      echo "\"$filename\" already exists, skipping."
+      echo -ne $COLOR_YELLOW
+      echo "\".$filename\" already exists, skipping."
+    else
+      echo -ne $COLOR_GREEN
+      echo "\".$filename\" is already linked"
     fi
   else
+    echo -ne $COLOR_GREEN
+    echo "\".$filename\" linked"
     ln -s "$original" "$symbolic"
   fi
 done
@@ -24,10 +35,18 @@ for filename in $(ls "$HOME/.localrcs"); do
   if [[ $target_host == $current_host ]]; then
     if [ -e "$symbolic" ]; then
       if [[ $(readlink "$symbolic") != "$original" ]]; then
+        echo -ne $COLOR_YELLOW
         echo "\".$filename\" already exists, skipping."
+      else
+        echo -ne $COLOR_GREEN
+        echo "\".$filename\" is already linked"
       fi
     else
+      echo -ne $COLOR_GREEN
+      echo "\".$filename\" linked"
       ln -s "$original" "$symbolic"
     fi
   fi
 done
+
+echo -ne $COLOR_NC
