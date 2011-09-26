8dc1c9631395928728c822fcf8f179f5f3a1b8bf
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-09-26 15:48:40 -0400

    DRY-up install script

--- install.sh
+++ install.sh
@@ -6,19 +6,9 @@ COLOR_YELLOW='\033[0;33m'
 
 EXCLUDE=(install.sh README.markdown)
 
-printf "Initializing and updating git submodules..."
-if (cd $(dirname $0) && exec git submodule sync &> /dev/null && exec git submodule update --init &> /dev/null); then
-  printf "\r$COLOR_GREEN"
-  printf "Submodules successfully initialized & updated.\n"
-else
-  printf "\r$COLOR_YELLOW"
-  printf "Submodules could not be initialized/updated.\n"
-fi
-
-for filename in $(ls "$(dirname $0)"); do
-  [[ ${EXCLUDE[@]} =~ $filename ]] && continue
-  original="$(cd "$(dirname $0)" && pwd)/$filename"
-  symbolic="$HOME/.$filename"
+linkfile() {
+  symbolic=$1;
+  original=$2;
 
   if [ -e "$symbolic" ]; then
     if [[ $(readlink "$symbolic") != "$original" ]]; then
@@ -33,6 +23,23 @@ for filename in $(ls "$(dirname $0)"); do
     printf "\".%s\" linked.\n" $filename
     ln -s "$original" "$symbolic"
   fi
+}
+
+printf "Initializing and updating git submodules..."
+if (cd $(dirname $0) && exec git submodule sync &> /dev/null && exec git submodule update --init &> /dev/null); then
+  printf "\r$COLOR_GREEN"
+  printf "Submodules successfully initialized & updated.\n"
+else
+  printf "\r$COLOR_YELLOW"
+  printf "Submodules could not be initialized/updated.\n"
+fi
+
+for filename in $(ls "$(dirname $0)"); do
+  [[ ${EXCLUDE[@]} =~ $filename ]] && continue
+  original="$(cd "$(dirname $0)" && pwd)/$filename"
+  symbolic="$HOME/.$filename"
+
+  linkfile $symbolic $original
 done
 
 mkdir -p ~/.vim/tmp ~/.vim/undo
@@ -44,19 +51,7 @@ for filename in $(ls "$HOME/.localrcs"); do
   target_host="$(printf $filename | cut -d'.' -f 1 | awk '{print tolower($0)}')"
 
   if [[ $target_host == $current_host ]]; then
-    if [ -e "$symbolic" ]; then
-      if [[ $(readlink "$symbolic") != "$original" ]]; then
-        printf $COLOR_YELLOW
-        printf "\".%s\" already exists, skipping.\n" $filename
-      else
-        printf $COLOR_GREEN
-        printf "\".%s\" is already linked.\n" $filename
-      fi
-    else
-      printf $COLOR_GREEN
-      printf "\".%s\" linked.\n" $filename
-      ln -s "$original" "$symbolic"
-    fi
+    linkfile $symbolic $original
   fi
 done
 
