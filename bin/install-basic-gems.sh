2288e821b0051fb963978a8969f9bd40ee410e76
Andrew Marshall <andrew@johnandrewmarshall.com>
2013-05-17 20:25:55 -0400

    Add script to install Ruby gems that I always want
    Mainly for whenever a new Ruby version is installed.

---
+++ bin/install-basic-gems.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+gems_with_doc=(
+  bundler
+  facets
+  mongoid
+  paint
+  rack
+  rails
+  rspec
+  sinatra
+)
+
+gems_without_doc=(
+  lunchy
+  pry
+  pry-debugger
+  pry-doc
+  pry-rescue
+  pry-theme
+)
+
+gem update --system
+gem update
+gem install gems_with_doc
+gem install --no-document gems_without_doc
+gem cleanup
