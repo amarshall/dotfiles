3b2678afe68f1a6bdda4b6883cb56822579033db
Andrew Marshall <andrew@johnandrewmarshall.com>
2014-03-03 21:33:38 -0500

    Update install-basic-gems.sh

--- bin/install-basic-gems.sh
+++ bin/install-basic-gems.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 
-gems_with_doc=(
+base_gems_with_doc=(
   bundler
   facets
-  mongoid
   paint
-  rack
-  rails
   rspec
-  sinatra
 )
 
-gems_without_doc=(
+base_gems_without_doc=(
   fruity
-  lunchy
   pry
-  pry-debugger
   pry-doc
-  pry-rescue
   pry-theme
 )
 
-gem update --system
-gem update
-gem install gems_with_doc
-gem install --no-document gems_without_doc
-gem cleanup
+extra_gems_with_doc=(
+  rack
+  rails
+  sinatra
+)
+
+extra_gems_without_doc=(
+  lunchy
+)
+
+update_rubygems() {
+  gem update --system
+  gem update
+}
+
+cleanup() {
+  gem cleanup
+}
+
+install_with_doc() {
+  gem install "$1"
+}
+
+install_without_doc() {
+  gem install --no-document --no-ri --no-rdoc "$1"
+}
+
+install_base_gems() {
+  for gem in "${base_gems_with_doc[@]}"; do install_with_doc "$gem"; done
+  for gem in "${base_gems_without_doc[@]}"; do install_without_doc "$gem"; done
+}
+
+install_extra_gems() {
+  for gem in "${extra_gems_with_doc[@]}"; do install_with_doc "$gem"; done
+  for gem in "${extra_gems_without_doc[@]}"; do install_without_doc "$gem"; done
+}
+
+set -e
+set -x
+
+update_rubygems
+install_base_gems
+[ "$1" = 'full' ] && install_extra_gems
+cleanup
