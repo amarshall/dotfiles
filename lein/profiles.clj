8c1f3de20b1cc00a7415776385d3e28d0b26e6d1
Andrew Marshall <andrew@johnandrewmarshall.com>
2014-07-08 13:34:44 -0400

    Add lein-ancient

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,9 @@
 {
+ :user {
+        :plugins [
+                  [lein-ancient "0.5.5"]
+                  ]
+        }
  :dev {
         :dependencies [[clj-stacktrace "0.2.8"]]
         :injections [(require 'clj-stacktrace.repl)]
