9cc0a7a493c892685340c59a96f4d2fb979bca2c
Andrew Marshall <andrew@johnandrewmarshall.com>
2014-10-04 01:41:32 -0400

    Formatting

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,14 +1,14 @@
 {
- :user {
-        :plugins [
-                  [lein-ancient "0.5.5"]
-                  ]
-        }
- :dev {
-        :dependencies [[clj-stacktrace "0.2.8"]]
-        :injections [(require 'clj-stacktrace.repl)]
-        :repl-options {
-                       :caught clj-stacktrace.repl/pst+
-                       }
-        }
- }
+  :user {
+    :plugins [
+      [lein-ancient "0.5.5"]
+    ]
+  }
+  :dev {
+    :dependencies [[clj-stacktrace "0.2.8"]]
+    :injections [(require 'clj-stacktrace.repl)]
+    :repl-options {
+      :caught clj-stacktrace.repl/pst+
+    }
+  }
+}
