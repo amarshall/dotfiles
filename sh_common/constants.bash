8d3d8d56c9a573115bb237f17658bbfc9ff677ea
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-05-04 17:38:47 -0400

    fzf: Use fd as default command if available
    fd is fast and ignores .gitignore files by default.

--- sh_common/constants.bash
+++ sh_common/constants.bash
@@ -36,3 +36,5 @@ fi
 
 BASE16_SHELL="$HOME/.sh_common/vendor/base16-shell/"
 [ -n "$PS1" ] && [ -s "$BASE16_SHELL/profile_helper.sh" ] && eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+type fd &> /dev/null && export FZF_DEFAULT_COMMAND='fd --type f'
