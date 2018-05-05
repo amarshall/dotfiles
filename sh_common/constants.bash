9620b325882d0c39170c5acf4f1901d5ab454187
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-05-05 03:29:02 -0400

    shell: Remove unused color constants and colors()
    Function is replaceable by the better terminal-colors
    <https://github.com/eikenb/terminal-colors>.

--- sh_common/constants.bash
+++ sh_common/constants.bash
@@ -11,25 +11,6 @@ export LESS="-i -M -S -R"
 export PAGER="$(command -vp less)"
 export VISUAL=vim
 
-# ANSI color constants
-export         COLOR_NONE='\033[0m'
-export        COLOR_WHITE='\033[1;37m'
-export        COLOR_BLACK='\033[0;30m'
-export         COLOR_BLUE='\033[0;34m'
-export   COLOR_LIGHT_BLUE='\033[1;34m'
-export        COLOR_GREEN='\033[0;32m'
-export  COLOR_LIGHT_GREEN='\033[1;32m'
-export         COLOR_CYAN='\033[0;36m'
-export   COLOR_LIGHT_CYAN='\033[1;36m'
-export          COLOR_RED='\033[0;31m'
-export    COLOR_LIGHT_RED='\033[1;31m'
-export       COLOR_PURPLE='\033[0;35m'
-export COLOR_LIGHT_PURPLE='\033[1;35m'
-export        COLOR_BROWN='\033[0;33m'
-export       COLOR_YELLOW='\033[1;33m'
-export         COLOR_GRAY='\033[1;30m'
-export   COLOR_LIGHT_GRAY='\033[0;37m'
-
 if command -v brew &>/dev/null; then
   export HOMEBREW_CASK_OPTS='--appdir=/Applications'
 fi
