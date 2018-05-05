0c106b1f8d211701c3add1e976495169ae0154f9
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-05-05 03:04:44 -0400

    shell: Fix broken command not found loading

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -7,5 +7,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # Pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-# For Ubuntu. Requires the "command_not_found" package be installed.
-[[ -d /etc/zsh_command_not_found ]] &&  . /etc/zsh_command_not_found
+# For Ubuntu. Requires the "command-not-found" package be installed.
+[[ -f /etc/zsh_command_not_found ]] &&  . /etc/zsh_command_not_found
