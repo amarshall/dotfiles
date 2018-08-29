f5e5112685b2efa75e4ef8632a7c98417786422e
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-08-29 17:42:47 -0400

    shell: Quote string with variable

--- lib/shell/plugins/base16/interactive.zsh
+++ lib/shell/plugins/base16/interactive.zsh
@@ -1,5 +1,5 @@
 BASE16_SHELL="$HOME/.config/dotfiles/shell/vendor/base16-shell/"
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$("$BASE16_SHELL"/profile_helper.sh)"
-if [[ -z "$SSH_CONNECTION" && "$SHLVL" -eq 1 ]]; then
+[ -n "$PS1" ] && [ -s "$BASE16_SHELL/profile_helper.sh" ] && eval "$("$BASE16_SHELL"/profile_helper.sh)"
+if [[ -z "$SSH_CONNECTION" ]]; then
   base16_solarized-dark
 fi
