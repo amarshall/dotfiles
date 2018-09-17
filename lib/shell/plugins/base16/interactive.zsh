af7c8ac1a1119cae656ef6d77efb7e0342d86a79
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-09-17 18:47:37 -0400

    shell: Don't reset base16 theme in nested shells

--- lib/shell/plugins/base16/interactive.zsh
+++ lib/shell/plugins/base16/interactive.zsh
@@ -1,8 +1,10 @@
-if [[ "$BASE16_THEME" == "base16-*" ]]; then
-  export BASE16_THEME="$(echo "$BASE16_THEME" | grep --extended-regexp --only '^base16-(.*)$')"
-fi
 BASE16_SHELL="$HOME/.config/dotfiles/shell/vendor/base16-shell/"
 [ -n "$PS1" ] && [ -s "$BASE16_SHELL/profile_helper.sh" ] && eval "$("$BASE16_SHELL"/profile_helper.sh)"
-if [[ -z "$SSH_CONNECTION" ]]; then
+
+if [[ "$BASE16_THEME" == base16-* ]]; then
+  export BASE16_THEME="$(echo "$BASE16_THEME" | perl -pe 's/^(base16-)+//')"
+fi
+
+if [[ -z "$SSH_CONNECTION" && $SHLVL = 1 ]]; then
   base16_solarized-dark
 fi
