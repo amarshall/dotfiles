af476dd3255a44a748717ce9b5c4c382866b8e75
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-12-06 13:47:46 -0500

    shell: Extract fzf config from core to plugin

--- lib/shell/plugins/_core/profile.zsh
+++ lib/shell/plugins/_core/profile.zsh
@@ -1,3 +1,2 @@
 export EDITOR="$(command -v nvim || command -v vim)"
 export VISUAL="$EDITOR"
-command -v fd &> /dev/null && export FZF_DEFAULT_COMMAND='fd --hidden --type f'
---
+++ lib/shell/plugins/fzf/profile.zsh
@@ -0,0 +1,1 @@
+command -v fd &> /dev/null && export FZF_DEFAULT_COMMAND='fd --hidden --type f'
