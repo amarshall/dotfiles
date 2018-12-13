fdc6fb00ae764f5a678051ed52a5a3dbf40b402a
Andrew Marshall <andrew@johnandrewmarshall.com>
2018-12-13 12:04:22 -0500

    zsh: Change fzf widget key bindings
    - <ctrl>T is tmux's binding, so would require hitting twice. <ctrl>J is
      accept-line, which is the same as hitting enter, so redundant.
    
    - <alt> is still mapped to macOS, so is not an option. <ctrl>H is
      backspace, so redundant.

--- lib/zsh/plugins/fzf/interactive.zsh
+++ lib/zsh/plugins/fzf/interactive.zsh
@@ -35,7 +35,7 @@ fzf-file-widget() {
   return $ret
 }
 zle     -N   fzf-file-widget
-bindkey '^T' fzf-file-widget
+bindkey '^J' fzf-file-widget
 
 # Ensure precmds are run after cd
 fzf-redraw-prompt() {
@@ -63,7 +63,7 @@ fzf-cd-widget() {
   return $ret
 }
 zle     -N    fzf-cd-widget
-bindkey '\ec' fzf-cd-widget
+bindkey '^H' fzf-cd-widget
 
 # CTRL-R - Paste the selected command from history into the command line
 fzf-history-widget() {
