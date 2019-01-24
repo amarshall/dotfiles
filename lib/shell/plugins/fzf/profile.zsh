ab9f10e435968dfb7e8f55566fb111db49ccb03f
Andrew Marshall <andrew@johnandrewmarshall.com>
2019-01-24 11:47:51 -0500

    fzf: Change color to mesh better with base16 themes
    In particular, base16-solarized-dark.

--- lib/shell/plugins/fzf/profile.zsh
+++ lib/shell/plugins/fzf/profile.zsh
@@ -1,1 +1,4 @@
-command -v fd &> /dev/null && export FZF_DEFAULT_COMMAND='fd --hidden --type f'
+if command -v fd &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='fd --hidden --type f'
+  export FZF_DEFAULT_OPTS='--color=fg+:18,bg+:20,hl:3,hl+:94,border:19,header:15,info:19,prompt:8,spinner:24'
+fi
