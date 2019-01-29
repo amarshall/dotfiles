2f433b6e42626b65e3c169b59013297eed21e322
Andrew Marshall <andrew@johnandrewmarshall.com>
2019-01-28 22:04:58 -0500

    fzf: Fix only setting default opts if fd installed

--- lib/shell/plugins/fzf/profile.zsh
+++ lib/shell/plugins/fzf/profile.zsh
@@ -1,4 +1,5 @@
+export FZF_DEFAULT_OPTS='--bind change:top --color=fg+:18,bg+:20,hl:3,hl+:94,border:19,header:15,info:19,prompt:8,spinner:24 --filepath-word'
+
 if command -v fd &> /dev/null; then
   export FZF_DEFAULT_COMMAND='fd --hidden --type f'
-  export FZF_DEFAULT_OPTS='--bind change:top --color=fg+:18,bg+:20,hl:3,hl+:94,border:19,header:15,info:19,prompt:8,spinner:24 --filepath-word'
 fi
