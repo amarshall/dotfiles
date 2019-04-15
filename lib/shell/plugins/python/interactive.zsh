9f2229e8055f1f2e990747f5b7d4146e7e4494e7
Andrew Marshall <andrew@johnandrewmarshall.com>
2019-04-15 15:06:59 -0400

    shell: Add pipenv wrapper to avoid creating Pipfiles everywhere
    Pipenv has the obnoxious behavior to create a `Pipfile` whenever some
    subcommands are run and there is `Pipfile`. Often this results in random
    `Pipfile`s being created when the command is run accidentally in the
    wrong directory. This avoids this by aborting if there's no `Pipfile`.

--- lib/shell/plugins/python/interactive.zsh
+++ lib/shell/plugins/python/interactive.zsh
@@ -1,3 +1,16 @@
 if command -v pyenv &> /dev/null; then
   eval "$(pyenv init -)"
 fi
+
+pipenv() {
+  if command -v pipenv &> /dev/null; then
+    if [[ -e Pipfile ]]; then
+      command pipenv "$@"
+    else
+      echo "No Pipfile present, aborting." >&2
+      return 1
+    fi
+  else
+    command pipenv
+  fi
+}
