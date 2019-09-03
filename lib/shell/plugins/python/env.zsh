36632d14acbe9bf65e69aed0bc6ca69c20033d98
Andrew Marshall <andrew@johnandrewmarshall.com>
2019-09-03 16:38:23 -0400

    shell: Fix looping over dirs that don’t exist
    Fails with glob error when the directory doesn’t exist, or it has no
    children.

--- lib/shell/plugins/python/env.zsh
+++ lib/shell/plugins/python/env.zsh
@@ -1,5 +1,6 @@
-for dir in "$HOME"/Library/Python/*; do
-  if [ -d "$dir/bin" ]; then
-    export PATH="$PATH:$dir/bin"
-  fi
-done
+if [ -d "$HOME"/Library/Python ]; then
+  find "$HOME"/Library/Python -mindepth 2 -maxdepth 2 -type d -name bin -print0 |
+    while IFS= read -r -d '' dir; do
+      export PATH="$PATH:$dir"
+    done
+fi
