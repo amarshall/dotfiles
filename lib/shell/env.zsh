8d600094975e22d4479ef0c9b47a5aaafa1f756b
Andrew Marshall <andrew@johnandrewmarshall.com>
2021-09-01 15:24:27 -0400

    shell: Ensure path_helper is exec-ed on Darwin

--- lib/shell/env.zsh
+++ lib/shell/env.zsh
@@ -1,3 +1,7 @@
+if [[ "$(uname)" == Darwin ]] && [ -x /usr/libexec/path_helper ]; then
+  eval "$(/usr/libexec/path_helper -s)"
+fi
+
 for dir in ~/.config/dotfiles/shell/plugins/*; do
   if [[ -f "$dir/env.zsh" ]]; then
     source "$dir/env.zsh"
